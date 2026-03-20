c8042465031f0fe067b2f945baae4ba77c722f7b
NgTriarii <krzysztof.nguyen.stud@pw.edu.pl>
2026-03-20 15:30:16 +0100

    Stage 2

---
+++ resource.h
@@ -0,0 +1,48 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PiGE_LAB3.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PIGELAB3_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_BASIC                       104
+#define IDM_CLEAR                       105
+#define IDI_PIGELAB3                    107
+#define IDI_SMALL                       108
+#define IDC_PIGELAB3                    109
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       129
+#define IDM_PROGRAMMER					130
+#define IDC_STATIC                      -1
+#define IDC_DISPLAY 100
+#define IDC_BTN_0   1000
+#define IDC_BTN_1   1001
+#define IDC_BTN_2   1002
+#define IDC_BTN_3   1003
+#define IDC_BTN_4   1004
+#define IDC_BTN_5   1005
+#define IDC_BTN_6   1006
+#define IDC_BTN_7   1007
+#define IDC_BTN_8   1008
+#define IDC_BTN_9   1009
+#define IDC_BTN_ADD 1010
+#define IDC_BTN_SUB 1011
+#define IDC_BTN_MUL 1012
+#define IDC_BTN_DIV 1013
+#define IDC_BTN_EQ  1014
+#define IDC_BTN_BS  1015
+#define IDC_BTN_C	1016
+#define IDC_BTN_DOT 1017
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
